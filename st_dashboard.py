7cb9fe0cbc7b9a017bca6c42b72fdf309d7dded8
RyanGN <ryanisrufus@hotmail.com>
2025-09-08 10:36:04 +0200

    uploaded heatmap and updated .py

--- st_dashboard.py
+++ st_dashboard.py
@@ -188,6 +188,17 @@ elif page == 'Interactive map with aggregated bike trips':
     st.markdown(" -**S 4 St & Wythe Ave**")
     st.markdown(" -**Pier 40 - Hudson River Park**")
 
+     st.header("Heatmap of Rental vs Deposit Mismatch")
+    st.image(
+        "04_Visualisations/Heatmap_NY_mapped.png",
+        use_column_width=True
+    )
+    st.markdown(
+        "This image repeats what is shown in the interactive map but via a heatmap, "
+        "and the dotted blue squares denote where the problematic areas may be."
+    )
+
+
 else:
     
     st.header("Conclusions and recommendations")
